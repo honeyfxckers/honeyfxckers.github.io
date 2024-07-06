0337c537f895551961a482b8d4a3cff19d9fa8c4
Artem <honeyfxckers@gmail.com>
2024-07-06 13:49:02 +0200

    123

--- JS/script.js
+++ JS/script.js
@@ -18,16 +18,32 @@ function updateEnergyBar() {
     }
 }
 
-function handleCoinClick() {
+function showPoints(points, x, y) {
+    const pointsElement = document.createElement('div');
+    pointsElement.className = 'points';
+    pointsElement.textContent = `+${points}`;
+    pointsElement.style.left = `${x}px`;
+    pointsElement.style.top = `${y}px`;
+    document.body.appendChild(pointsElement);
+
+    setTimeout(() => {
+        pointsElement.remove();
+    }, 1000);
+}
+
+function handleCoinClick(event) {
     if (energy > 0) {
         score++;
         energy -= 1;
         scoreDisplay.textContent = score;
         updateEnergyBar();
+        const x = event.clientX || event.touches[0].clientX;
+        const y = event.clientY || event.touches[0].clientY;
+        showPoints(1, x, y); // Показываем число 1 при каждом клике
         if (navigator.vibrate) {
             navigator.vibrate(50); // Вибрация на 50 миллисекунд
         }
-        coin.style.transform = 'scale(0.9)'; // Уменьшение изображения
+        coin.style.transform = 'scale(0.95)'; // Уменьшение изображения
         setTimeout(() => {
             coin.style.transform = 'scale(1)'; // Возвращение к исходному размеру
         }, 100);
@@ -37,7 +53,7 @@ function handleCoinClick() {
 coin.addEventListener('click', handleCoinClick);
 coin.addEventListener('touchstart', (event) => {
     event.preventDefault(); // Предотвращение всплытия событий на мобильных устройствах
-    handleCoinClick();
+    handleCoinClick(event);
     if (navigator.vibrate) {
         navigator.vibrate(50); // Вибрация на 50 миллисекунд
     }
